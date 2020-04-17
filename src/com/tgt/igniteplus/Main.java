77bde7c7e98e8a4f57f666cfc16fbda255156351
somya1209 <somya123024@gmail.com>
2020-04-17 15:29:50 +0530

    checking deliverability in an area using zipcode

--- src/com/tgt/igniteplus/Main.java
+++ src/com/tgt/igniteplus/Main.java
@@ -1,8 +1,33 @@
 package com.tgt.igniteplus;
 
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
+
+   static void deliverability(int zipCode){
+      Set<Integer> nonDeliverableZipCodes=new LinkedHashSet<>();
+      nonDeliverableZipCodes.add(123);
+      nonDeliverableZipCodes.add(456);
+      nonDeliverableZipCodes.add(789);
+      try {
+         if(nonDeliverableZipCodes.contains(zipCode)) throw new MyException(String.format("Not yet delivering at " + zipCode));
+         System.out.println("Delivery available in your area! ");
+      } catch (MyException e) {
+         System.out.println("Caught exception");
+         System.out.println(e.getMessage());
+      }
+
+
+   }
+
    public static void main(String[] args){
+      Scanner in =new Scanner(System.in);
+      System.out.print("Enter zip code to check availability : ");
+      int zipCode=in.nextInt();
+      deliverability(zipCode);
 
    }
 }
---
+++ src/com/tgt/igniteplus/MyException.java
@@ -0,0 +1,7 @@
+package com.tgt.igniteplus;
+
+public class MyException extends Exception {
+    public MyException(String s){
+        super(s);
+    }
+}
